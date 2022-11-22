be51a7b18d74ce0a9d7e4a1ecc7736dac5cc0163
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2022-11-22 18:40:39 +1100

    Updated Websites

--- resources/views/pages/websites.blade.php
+++ resources/views/pages/websites.blade.php
@@ -3,19 +3,43 @@
         <div class="bg-black-50">
             <div class="content content-top content-full text-center">
                 <h1 class="fw-bold text-white mt-5 mb-2">
-                    Check out our latest stories
+                    Elegant Website Solutions
                 </h1>
-                <h3 class="fw-normal text-white-75 mb-5">Be inspired and create something amazing today.</h3>
+                <h3 class="fw-normal text-white-75 mb-5">Assembling your Online Presence, Specially Tailored for your
+                    Business</h3>
             </div>
         </div>
     </div>
     <div id="main" class="content">
-        <h2 class="content-heading">Page Subtitle</h2>
-        <p>Your content..</p>
+        <h2 class="content-heading">Our Commitment To You</h2>
+        <p>We find pride in un-earthing the qualities of your business in the online world. You will find attention to
+            detail along every step of the design process, considering our keen eye for what makes your business unique. We
+            will cater to providing your services in-site, from small single page bookings to larger online shops. Our
+            mastering process provides a robust foundation for expanding the market reach of your business, procuring more
+            impressions from more customers.</p>
 
-
+        <h2 class="content-heading">Our Clients</h2>
         <div class="row">
+            <div class="col-md-4 hover-container">
+                <a class="block block-rounded bg-image minecraft-covers website-clients"
+                    style="background-image: url('/media/photos/feverdreams-mainbanner.jpg');"
+                    href="https://www.feverdreams.com.au/">
+                    <div class="block-content block-content-full ratio ratio-16x9">
+                    </div>
+                </a>
+                <a class="block block-rounded bg-image minecraft-covers website-clients hover-image"
+                    style="background-image: url('/media/photos/feverdreams-banner.png');"
+                    href="https://www.feverdreams.com.au/">
+                    <div class="block-content block-content-full ratio ratio-16x9">
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
 
+            </div>
+            <div class="col-md-4">
+
+            </div>
         </div>
     </div>
 @endsection
